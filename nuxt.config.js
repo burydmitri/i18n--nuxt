25d6e7ea198fce320d7f32eb883811251f9d7ee8
Дмитрий <burydima28122003@gmail.com>
2020-09-06 20:41:25 +0300

    add css variables and some styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: ['normalize.css/normalize.css'],
+  css: ['normalize.css/normalize.css', '~assets/styles/variables.css'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
-    <h2>{{ $t('hello') }}</h2>
+    <h2 class="home__title">{{ $t('hello') }}</h2>
     <div class="buttons home__buttons">
-      <nuxt-link :to="switchLocalePath('ru')" class="buttons--ru">{{
-        $t('buttons.ru')
-      }}</nuxt-link>
-      <nuxt-link :to="switchLocalePath('en')" class="buttons--en">{{
-        $t('buttons.en')
-      }}</nuxt-link>
+      <nuxt-link
+        :to="switchLocalePath('ru')"
+        class="buttons__btn buttons__btn--ru"
+        >{{ $t('buttons.ru') }}</nuxt-link
+      >
+      <nuxt-link
+        :to="switchLocalePath('en')"
+        class="buttons__btn buttons__btn--en"
+        >{{ $t('buttons.en') }}</nuxt-link
+      >
     </div>
   </div>
 </template>
@@ -25,5 +29,36 @@ export default {}
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  color: var(--white);
+  background: var(--black);
+
+  &__title {
+    font-size: 50px;
+
+    margin-bottom: 50px;
+  }
+}
+.buttons {
+  display: flex;
+  gap: 20px;
+
+  &__btn {
+    min-width: 100px;
+    min-height: 50px;
+
+    display: block;
+
+    color: var(--white);
+    background-color: var(--red);
+    text-decoration: none;
+    transition: 0.1s;
+
+    padding: 20px;
+
+    &:hover {
+      background-color: darken(#f47265, 1 0%);
+    }
+  }
 }
 </style>
